7bb235177ce30236c9cdeecb0870e65848c5dd6c
Ilia Kondrashov <caseycs@gmail.com>
2014-11-09 10:14:09 +0100

    comments

--- index.php
+++ index.php
@@ -11,6 +11,15 @@ $rank->rankStyle = $rankStyle;
 
 $rankStyle->rank = $rank;
 
-$entityManager->persist($rankStyle);
+//this will create both entities in DB, here is an explanation:
+//http://doctrine-orm.readthedocs.org/en/latest/reference/working-with-associations.html#transitive-persistence-cascade-operations
+//in 2 words: cascade at least on creating new entities works only from owner to children, in our case - rankStyle is the owner
 
+$entityManager->persist($rankStyle);
 $entityManager->flush();
+
+/**
+this code will fail:
+$entityManager->persist($rank);
+$entityManager->flush();
+ */
